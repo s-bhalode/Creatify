0e32928897d7f3ff5ef5778c8fe89135bc310869
Simran Bhalode <simran.bhalode@gmail.com>
2022-07-21 14:33:28 +0530

    Designer Post UI done

--- src/app/designer/designer-post/designer-post.component.ts
+++ src/app/designer/designer-post/designer-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {DesignerGalleryService} from '../../services/designer-gallery.service';
 
 @Component({
   selector: 'app-designer-post',
@@ -7,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignerPostComponent implements OnInit {
 
-  constructor() { }
+  selectedFile? : FileList;
+  currentFile?: File;
+  selectedImage! : string;
+
+  constructor(private dialog : MatDialog,
+              private router : Router,
+              private designerGalleryService : DesignerGalleryService) { }
 
   ngOnInit(): void {
     
   }
+
+  designerPostForm = new FormGroup({
+    caption : new FormControl(''),
+    image : new FormControl(''),
+  })
+
+  designerPostImageForm = new FormGroup({
+    postImage : new FormControl('', [Validators.required])
+  })
+
+  get postImage(){
+    return this.designerPostImageForm.controls;
+  }
+
+  pickedImage(event : any){
+      this.selectedFile = event.target.files ;
+      if(this.selectedFile){
+        const file : File | null = this.selectedFile.item(0);
+
+        if(file){
+          this.currentFile = file;
+          this.designerPostForm.patchValue({image : this.currentFile});
+          this.designerPostForm.get('image')?.updateValueAndValidity();
+          const reader = new FileReader();
+          reader.onload = () => {
+            this.selectedImage = reader.result as string;
+          };
+          reader.readAsDataURL(this.currentFile);
+        }
+      }   
+  }
+
+  onSubmit(formObj : any){
+
+  }
+
+
 
 }
---
+++ src/app/services/designer-gallery.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class DesignerGalleryService {
+  
+  API_URL = "http://localhost:3000/designer/designer-post/";
+
+  constructor(private http : HttpClient) { }
+
+  
+}

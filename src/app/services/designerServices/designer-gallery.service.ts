24cc2d710ce2ad9286129fb0e96fa116b55f4763
Simran Bhalode <simran.bhalode@gmail.com>
2022-07-24 13:39:45 +0530

    UI of designer-post completed

--- src/app/services/designerServices/designer-gallery.service.ts
+++ src/app/services/designerServices/designer-gallery.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,11 +9,23 @@ import { Observable } from 'rxjs';
 
 export class DesignerGalleryService {
   
-  API_URL = "http://localhost:3000/designer/designer-post/";
+  API_URL = "http://localhost:3000/designer/designer-post";
 
   constructor(private http : HttpClient) { }
 
-  
+  uploadPost(file : File, caption : any) : Observable<HttpEvent<any>>{
+    const formData : FormData = new FormData();
+    formData.append('file', file);
+    formData.append('caption', caption);
+    const req = new HttpRequest('POST', `${this.API_URL}/upload`, formData, {
+      reportProgress : true,
+      responseType : 'json'
+    });
+    return this.http.request(req);
+  }
 
+  getFiles() : Observable<any>{
+    return this.http.get(`${this.API_URL}/posts`);
+  }
   
 }
